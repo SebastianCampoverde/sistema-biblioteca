fbac0e2cd513723fe6e5811686f1924d8f10e2df
Fernando Santacruz <94269482+lfSant@users.noreply.github.com>
2023-07-13 23:49:25 -0500

    fix: branch start

--- src/main/java/com/ucacue/backendlibros/infraestructure/repository/EstudianteRepository.java
+++ src/main/java/com/ucacue/backendlibros/infraestructure/repository/EstudianteRepository.java
@@ -2,6 +2,9 @@ package com.ucacue.backendlibros.infraestructure.repository;
 
 import com.ucacue.backendlibros.model.Estudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//Esto es un repositorio
+@Repository
 public interface EstudianteRepository extends JpaRepository<Estudiante, Integer> {
 }
